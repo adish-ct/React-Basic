d61542df185f86b626a8f09ba6f9e2ac93c49bd7
adish-ct <adishathu525@gmail.com>
2023-10-09 17:17:23 +0530

    created counter file

---
+++ counter.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+
+function SubmitButton() {
+    const [count, setCount] = useState(0)
+    const [color, setColor] = useState(0)
+
+    const decreaseCount = () => {
+        setCount(count - 1);
+        setColor(-1)
+    }
+
+    const addCount = () => {
+        setCount(count + 1);
+        setColor(1)
+    }
+
+
+    return (
+        <div>
+            <hr />
+            <h1>Count : <span className={color === 1 ? "text-success" : "text-danger"}>{count}</span> </h1>
+            <div className="d-flex gap-5 justify-content-center mt-5">
+                <button className="btn btn-lg btn-danger" onClick={decreaseCount} > Decrease </button>
+                <button className="btn btn-lg btn-success" onClick={addCount} > Increase </button>
+            </div>
+        </div>
+    )
+}
+
+export default SubmitButton
